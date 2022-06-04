15fe03b06cc73cfe74efc50e823dc49537c166ff
Felkng <105019177+Felkng@users.noreply.github.com>
2022-06-04 11:08:50 -0300

    Create for_loop.java

---
+++ for_loop.java
@@ -0,0 +1,13 @@
+package Java_iniciaçao;
+
+public class for_loop
+{
+    public static void main(String[] args)
+    {
+        int count;
+        //for(INICIALIZAÇÃO; CONDIÇÃO; INTERAÇÃO(O QUE É FEITO))
+        for(count = 0; count <5; count++) //SE O RESULTADO DO TESTE É VERDADEIRO, A DECLARAÇÃO É EXECUTADA E O "FOR LOOP" CONTINUAR A INTERAGIR. SE A DECLARAÇÃO É FALSA, O LOOP SE ENCERRA
+        System.out.println("This is count: " + count);
+        System.out.println("Done!");
+    }
+}
